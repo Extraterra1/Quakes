0d57c74bbd6a3643fe88651d63d5742c4d136ea4
artxE <extraterra1@gmail.com>
2020-04-02 14:36:29 +0100

    new layout

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
 const express = require("express");
 const app = express();
-const React = require('react');
-const Rating = require('react-rating');
-const http = require('https');
-const request = require('request');
-const moment = require('moment');
+const request = require("request");
+const moment = require("moment");
 
-app.set("view engine", "ejs");
+app.set("view engine", "pug");
+app.use(express.static("public"));
 
-app.get('/', function(req,res){
-    var currentDay = moment(Date.now()).format('YYYY-MM-DD');
-    var nextDay = moment(currentDay).add(1, 'days').format('YYYY-MM-DD');
-    var url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + currentDay + '&endtime=' + nextDay + '&minmagnitude=5';
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var parsedData = JSON.parse(body);
-            res.render("index", {
-                data: parsedData,
-                moment: moment,
-                currentDay: currentDay
-            });
-        }
-    });
+app.get("/", function(req, res) {
+  var currentDay = moment(Date.now()).format("YYYY-MM-DD");
+  var nextDay = moment(currentDay)
+    .add(1, "days")
+    .format("YYYY-MM-DD");
+  var url =
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" +
+    currentDay +
+    "&endtime=" +
+    nextDay +
+    "&minmagnitude=5";
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedData = JSON.parse(body);
+      res.render("verti", {
+        data: parsedData,
+        moment: moment,
+        currentDay: currentDay
+      });
+    }
+  });
 });
 
-app.get("/:startdate/:enddate", function(req,res){
-    var currentDay = req.params.startdate
-    var nextDay = req.params.enddate;
-    var url = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=' + currentDay + '&endtime=' + nextDay + '&minmagnitude=5';
-    request(url, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            var parsedData = JSON.parse(body);
-            res.render("index", {
-                data: parsedData,
-                moment: moment,
-                currentDay: currentDay
-            });
-        }
-    });
-
-})
+app.get("/:startdate/:enddate", function(req, res) {
+  var currentDay = req.params.startdate;
+  var nextDay = req.params.enddate;
+  var url =
+    "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=" +
+    currentDay +
+    "&endtime=" +
+    nextDay +
+    "&minmagnitude=5";
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var parsedData = JSON.parse(body);
+      res.render("verti", {
+        data: parsedData,
+        moment: moment,
+        currentDay: currentDay
+      });
+    }
+  });
+});
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("server listening");
-})
+  console.log("server listening");
+});
